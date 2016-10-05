8a7f97780e4c61b3e5f3f7b4ffe7d045cc19fe7d
Samuel Parkinson <sam.james.parkinson@gmail.com>
2016-10-05 23:55:07 +0100

    Initial commit :rocket:.

---
+++ README.md
@@ -0,0 +1,3 @@
+# gatekeeper-alexa-skill [![Build Status](https://travis-ci.org/sjparkinson/gatekeeper-alexa-skill.svg?branch=master)](https://travis-ci.org/sjparkinson/gatekeeper-alexa-skill)
+
+:speech_balloon: A Smart Home Alexa skill for the Gatekeeper IoT project.
---
+++ lambda_function.py
@@ -0,0 +1,123 @@
+import json
+import logging
+import requests
+import uuid
+
+PARTICLE_CLOUD_API_ENDPOINT = 'https://api.particle.io'
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+
+# Main function entrypoint, handles request routing.
+def lambda_handler(event, context):
+    logger.info("Recieved event: {}".format(json.dumps(event)))
+
+    # The Smart Home API request namespace.
+    namespace = event['header']['namespace']
+
+    if namespace == 'Alexa.ConnectedHome.Discovery':
+        return handle_device_discovery(namespace)
+
+    if namespace == 'Alexa.ConnectedHome.Control':
+        if event['header']['name'] == 'TurnOnRequest':
+            return handle_turn_on_control(namespace, event['payload'])
+
+        if event['header']['name'] == 'HealthCheckRequest':
+            return handle_health_check(namespace, event['payload'])
+
+
+def handle_device_discovery(namespace):
+    logger.info('Handling discovery event.')
+
+    headers = {
+        'messageId': str(uuid.uuid4()),
+        'name': 'DiscoverApplicancesResponse',
+        'namespace': namespace,
+        'payloadVersion': '2',
+    }
+
+    payload = {
+        'discoveredAppliances': [
+            {
+                'applianceId': '20003a000747343232363230',
+                'manufacturerName': 'Particle Industries, Inc.',
+                'modelName': 'Particle Photon',
+                'version': 'v1',
+                'friendlyName': 'gatekeeper',
+                'friendlyDescription': 'Gatekeeper connected via Particle Cloud',
+                'isReachable': True,
+                'actions': [
+                    'turnOn'
+                ],
+                'additionalApplianceDetails': {}
+            }
+        ]
+    }
+
+    response = {
+        'header': headers,
+        'payload': payload,
+    }
+
+    logger.info("Returning discovery response: {}".format(json.dumps(response)))
+
+    return response
+
+
+def handle_turn_on_control(namespace, payload):
+    logger.info('Handling control request.')
+
+    # The OAuth2 access token for access to the Particle Cloud API.
+    access_token = payload['accessToken']
+
+    # The Particle device ID.
+    device_id = payload['applianceId']
+
+    # Call the `prime` function on the device.
+    url = PARTICLE_CLOUD_API_ENDPOINT + '/v1/devices/{}/prime'.format(device_id)
+    requests.post(url, data={
+        'arg': '',  # Required parameter that isn't used.
+        'access_token': access_token,
+    })
+
+    headers = {
+        'messageId': str(uuid.uuid4()),
+        'name': 'TurnOnConfirmation',
+        'namespace': namespace,
+        'payloadVersion': '2',
+    }
+
+    response = {
+        'header': headers,
+        'payload': {},
+    }
+
+    logger.info("Returning control response: {}".format(json.dumps(response)))
+
+    return response
+
+
+def handle_health_check(namespace):
+    logger.info('Handling health check request.')
+
+    headers = {
+        'messageId': str(uuid.uuid4()),
+        'name': 'TurnOnConfirmation',
+        'namespace': namespace,
+        'payloadVersion': '2',
+    }
+
+    payload = {
+        'description': 'Gatekeeper is probably online',
+        'isHealthy': True,
+    }
+
+    response = {
+        'header': headers,
+        'payload': payload,
+    }
+
+    logger.info("Returning health check response: {}".format(json.dumps(response)))
+
+    return response
