1d7b2f02f0571912100cfa722eaebb67f44b3257
Samuel Parkinson <sjparkinson@users.noreply.github.com>
2016-10-09 15:45:02 +0100

    Add application ID check.

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,7 @@ import logging
 import requests
 import uuid
 
+APPLICATION_ID = '@@APPLICATION_ID@@'
 PARTICLE_CLOUD_API_ENDPOINT = 'https://api.particle.io'
 
 logger = logging.getLogger()
@@ -12,6 +13,10 @@ logger.setLevel(logging.DEBUG)
 # Main function entrypoint, handles request routing.
 def lambda_handler(event, context):
     logger.info("Recieved event: {}".format(json.dumps(event)))
+    
+    # Ensure we're recieving an event from the correct Alexa skill.
+    if event['session']['application']['applicationId'] != APPLICATION_ID:
+        raise ValueError('The application ID was not valid.') 
 
     # The Smart Home API request namespace.
     namespace = event['header']['namespace']
